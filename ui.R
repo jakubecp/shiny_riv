b794ed3d9f3da952aa64e0aa86784a8796f6ebd0
jakubecp <jakubecp@fzp.czu.cz>
2016-06-12 10:13:57 +0200

    changed starting values to 1

--- ui.R
+++ ui.R
@@ -14,13 +14,13 @@ shinyUI(fluidPage(
   fluidRow(
     column(4, tags$h4("Provide values from InCites - Journal Citation Reports"), tags$a(href="https://jcr-incites-thomsonreuters-com.infozdroje.czu.cz/JCRJournalHomeAction.action?SID=A1-rqLofhhSS3hxxWoNjiiggonsJcu8g19rr-18x2dZWgQi1ONLdEPrQKQ8eH2vwx3Dx3D5upqzgx2BTI1d7uB0rIqKnGAx3Dx3D-9vvmzcndpRgQCGPd1c2qPQx3Dx3D-wx2BJQh9GKVmtdJw3700KssQx3Dx3D&refineString=null&SrcApp=IC2LS&timeSpan=null&Init=Yes&wsid=Y2jqz6wpKoxOgL7yixj", "Web of Science"), tags$br(),tags$br(),
       numericInput("cat1",
-        "Number of journals at the first category:", 0,
+        "Number of journals at the first category:", 1,
         min = 1,
         max = 5000),
       
       
       numericInput("jour1",
-                  "Ranking in the first category:", 0,
+                  "Ranking in the first category:", 1,
                   min = 1,
                   max = 5000),
       
