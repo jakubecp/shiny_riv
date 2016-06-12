d8b5dbdfb6be246bc09ffb06d21371588f936138
jakubecp <jakubecp@fzp.czu.cz>
2016-06-12 19:39:21 +0200

    invalid values were baned by if else statements

--- server.R
+++ server.R
@@ -15,12 +15,22 @@ shinyServer(function(input, output) {
     n1 <- (input$jour1-1)/(input$cat1-1)
     f1 <- (1-n1)/(1+(n1/0.057))
     r1 <- 10+295*f1
-    if (r1 < 10) {print ("Please, provide meaningful values")
+    
+    val <- is.numeric(r1)
+    journal1 <- is.integer(input$jour1)
+    cat1 <- is.integer(input$cat1)
+    check1 <- input$jour1 - input$cat1
+    
+    if (input$jour1 < 1.000) {print ("Journal rank cannot be smaller then one!")
       
-    } else if (r1 > 305.000) {print ("Please, provide meaningful values")
+    } else if (journal1 == FALSE) {print ("Please, provide meaningful values")
+      
+    } else if (cat1 == FALSE) {print ("Please, provide meaningful values")
+      
+    } else if (check1 > 0) {print ("Rank of the journal is bigger then number of journals in its category!")
       
     } else { 
-      print(r1)
+      print(10+295*f1)
       
     }  
     
--- ui.R
+++ ui.R
@@ -14,8 +14,8 @@ shinyUI(fluidPage(
   fluidRow(
     sidebarPanel(tags$h4("Provide values from InCites - Journal Citation Reports"), tags$a(href="https://jcr-incites-thomsonreuters-com.infozdroje.czu.cz/JCRJournalHomeAction.action?SID=A1-rqLofhhSS3hxxWoNjiiggonsJcu8g19rr-18x2dZWgQi1ONLdEPrQKQ8eH2vwx3Dx3D5upqzgx2BTI1d7uB0rIqKnGAx3Dx3D-9vvmzcndpRgQCGPd1c2qPQx3Dx3D-wx2BJQh9GKVmtdJw3700KssQx3Dx3D&refineString=null&SrcApp=IC2LS&timeSpan=null&Init=Yes&wsid=Y2jqz6wpKoxOgL7yixj", "Web of Science"), tags$br(),tags$br(),
       numericInput("cat1",
-        "Number of journals at the first category:", 2,
-        min = 2,
+        "Number of journals at the first category:", 1,
+        min = 1,
         max = 5000),
       
       
