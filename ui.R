745a7906857418257ff3929f29e66f3b53985add
jakubecp <jakubecp@fzp.czu.cz>
2016-06-14 12:26:40 +0200

    web link will pop up in new window

--- ui.R
+++ ui.R
@@ -12,7 +12,7 @@ shinyUI(fluidPage(
 
   # Sidebar with a number of numeric inputs
   fluidRow(
-    sidebarPanel(tags$h4("Provide values from InCites - Journal Citation Reports"), tags$a(href="https://jcr-incites-thomsonreuters-com.infozdroje.czu.cz/JCRJournalHomeAction.action?SID=A1-rqLofhhSS3hxxWoNjiiggonsJcu8g19rr-18x2dZWgQi1ONLdEPrQKQ8eH2vwx3Dx3D5upqzgx2BTI1d7uB0rIqKnGAx3Dx3D-9vvmzcndpRgQCGPd1c2qPQx3Dx3D-wx2BJQh9GKVmtdJw3700KssQx3Dx3D&refineString=null&SrcApp=IC2LS&timeSpan=null&Init=Yes&wsid=Y2jqz6wpKoxOgL7yixj", "Web of Science"), tags$br(),tags$br(),
+    sidebarPanel(tags$h4("Provide values from InCites - Journal Citation Reports"), tags$a(href="https://jcr-incites-thomsonreuters-com.infozdroje.czu.cz/JCRJournalHomeAction.action?SID=A1-rqLofhhSS3hxxWoNjiiggonsJcu8g19rr-18x2dZWgQi1ONLdEPrQKQ8eH2vwx3Dx3D5upqzgx2BTI1d7uB0rIqKnGAx3Dx3D-9vvmzcndpRgQCGPd1c2qPQx3Dx3D-wx2BJQh9GKVmtdJw3700KssQx3Dx3D&refineString=null&SrcApp=IC2LS&timeSpan=null&Init=Yes&wsid=Y2jqz6wpKoxOgL7yixj", "Web of Science", target="blank"), tags$br(),tags$br(),
       numericInput("cat1",
         "Number of journals at the first category:", 1,
         min = 1,
@@ -24,42 +24,42 @@ shinyUI(fluidPage(
                   min = 1,
                   max = 5000),
       
-      tags$h5 ("RIV point calculation for journals in multiple categories will be integrated soon..."),
+      tags$h5 ("RIV point calculation for journals in multiple categories will be integrated soon...")
       
-      #two categories
-      numericInput("cat2",
-        "Number of journals at the second category:", 1,
-        min = 1,
-        max = 5000),
-      numericInput("jour2",
-        "Ranking in the second category:", 1,
-        min = 1,
-        max = 5000),
-      
-      #three categories
-      numericInput("cat3",
-        "Number of journals at the third category:", 1,
-        min = 1,
-        max = 5000),
-      
-      
-      numericInput("jour3",
-        "Ranking in the third category:", 1,
-        min = 1,
-        max = 5000),
-      
-      #four categories
-      numericInput("cat4",
-        "Number of journals at the fourth category:", 1,
-        min = 1,
-        max = 5000),
-      
-      
-      numericInput("jour4",
-        "Ranking in the fourth category:", 1,
-        min = 1,
-        max = 5000)
-      
+      # #two categories
+      # numericInput("cat2",
+      #   "Number of journals at the second category:", 1,
+      #   min = 1,
+      #   max = 5000),
+      # numericInput("jour2",
+      #   "Ranking in the second category:", 1,
+      #   min = 1,
+      #   max = 5000),
+      # 
+      # #three categories
+      # numericInput("cat3",
+      #   "Number of journals at the third category:", 1,
+      #   min = 1,
+      #   max = 5000),
+      # 
+      # 
+      # numericInput("jour3",
+      #   "Ranking in the third category:", 1,
+      #   min = 1,
+      #   max = 5000),
+      # 
+      # #four categories
+      # numericInput("cat4",
+      #   "Number of journals at the fourth category:", 1,
+      #   min = 1,
+      #   max = 5000),
+      # 
+      # 
+      # numericInput("jour4",
+      #   "Ranking in the fourth category:", 1,
+      #   min = 1,
+      #   max = 5000)
+      # 
       
       
       
